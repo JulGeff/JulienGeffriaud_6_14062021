ceb3356530f5811cdaa1d04fbd86cff5c84b592a
JulGeff <julien.geffriaud.pro@gmail.com>
2021-06-24 16:20:11 +0200

    dotend token correction

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-
+require('dotenv').config()
+const hiddenToken = process.env.TOKEN;
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -33,7 +34,7 @@ exports.login = (req, res, next) => {
               userId: user._id,
               token: jwt.sign(
                 { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
+                hiddenToken,
                 { expiresIn: '24h' }
               )
             });
